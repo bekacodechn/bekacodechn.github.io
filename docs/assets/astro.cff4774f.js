73d10a4599ab718ac39444714a6e97ab7bece6ae
bekacodechn <bekacodechn@gmail.com>
2023-11-02 21:42:02 +0800

    模板项目

---
+++ docs/assets/astro.cff4774f.js
@@ -0,0 +1,5 @@
+import{jsx as e,jsxs as o,Fragment as a}from"react/jsx-runtime";const s=void 0,i=[],d="Astro";function r(n){const t=Object.assign({h1:"h1",a:"a",p:"p"},n.components);return o(a,{children:[o(t.h1,{id:"astro",children:[e(t.a,{className:"header-anchor","aria-hidden":"true",href:"#astro",children:"#"}),"Astro"]}),`
+`,e(t.p,{children:"123123"})]})}function h(n={}){const{wrapper:t}=n.components||{};return t?e(t,Object.assign({},n,{children:e(r,n)})):r(n)}const l="Invalid Date",m=`# Astro
+
+123123
+`;export{m as content,h as default,s as frontmatter,l as lastUpdatedTime,d as title,i as toc};
